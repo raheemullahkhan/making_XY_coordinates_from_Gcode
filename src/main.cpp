30634bdef953ea32c6fdcc0a19dede6c15c100c1
raheemullahkhan <rehan38k@gmail.com>
2024-02-27 20:23:38 +0500

    the value gcode is store as unit32_t

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include<Arduino.h>
+uint16_t count_coordinate=0;
+uint32_t xdata[200];
+uint32_t ydata[200];
+
 
 void separateCoordinates(String gcode);
 void setup() {
@@ -15,8 +19,13 @@ void setup() {
 
   // Separate X and Y components
   separateCoordinates(gcode);
+  Serial.println("x and y data in array");
+  for(int i=0;i<count_coordinate;i++)
+ { Serial.print((float)xdata[i]/10000,4);
+  Serial.print("          ");
+  Serial.println((float)ydata[i]/10000,4);
 }
-
+}
 void loop() {}
 
 void separateCoordinates(String gcode) {
@@ -25,7 +34,8 @@ void separateCoordinates(String gcode) {
 
   char* token = strtok(cstr, " ");
   bool firstCoord = true;
-  while (token != NULL) {
+  while (token != NULL) 
+  {
     if (token[0] == 'X') {
       float x = atof(token + 1);
       if (!firstCoord) {
@@ -33,11 +43,13 @@ void separateCoordinates(String gcode) {
       }
       Serial.print("(");
       Serial.print(x, 4); // Print x with 4 decimal places
+      xdata[count_coordinate]=x*10000;
       firstCoord = false;
     } else if (token[0] == 'Y') {
       float y = atof(token + 1);
       Serial.print(",");
       Serial.print(y, 4); // Print y with 4 decimal places
+      ydata[count_coordinate++]=y*10000;
       Serial.print(")");
       firstCoord = true;
     }
