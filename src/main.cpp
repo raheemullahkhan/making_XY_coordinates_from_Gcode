5cd62a35ac2cd35ea6e711df7def75bf993bb2dd
raheemullahkhan <rehan38k@gmail.com>
2024-02-27 16:01:32 +0500

    changing one pattern to xy coordinates

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include<Arduino.h>
+void separateCoordinates(String gcode);
+void setup() {
+
+  
+  Serial.begin(9600);
+  
+  // Wait for user input
+  Serial.println("Enter G-code:");
+  while (!Serial.available()) {
+    // Wait for input
+  }
+  
+  // Read the entered G-code
+  String gcode = Serial.readStringUntil('\n');
+
+  // Separate X and Y components
+  separateCoordinates(gcode);
+}
+
+void loop() {}
+
+void separateCoordinates(String gcode) {
+  char* cstr = new char[gcode.length() + 1];
+  strcpy(cstr, gcode.c_str());
+
+  char* token = strtok(cstr, " ");
+  bool firstCoord = true;
+  while (token != NULL) {
+    if (token[0] == 'X') {
+      float x = atof(token + 1);
+      if (!firstCoord) {
+        Serial.print(",");
+      }
+      Serial.print("(");
+      Serial.print(x);
+      firstCoord = false;
+    } else if (token[0] == 'Y') {
+      float y = atof(token + 1);
+      Serial.print(",");
+      Serial.print(y);
+      Serial.print(")");
+      firstCoord = true;
+    }
+    token = strtok(NULL, " ");
+  }
+
+  delete[] cstr;
+}
